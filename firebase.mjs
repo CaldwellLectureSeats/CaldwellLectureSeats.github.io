a4d47331c0f11e2a00750ce0bc98d988c8e6599b
vdv7 <vdv7@users.noreply.github.com>
2024-02-05 12:16:36 -0500

    domain

--- firebase.mjs
+++ firebase.mjs
@@ -1,1 +1,388 @@
-import{initializeApp}from"https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";import{getAnalytics}from"https://www.gstatic.com/firebasejs/10.7.0/firebase-analytics.js";import{getAuth,onAuthStateChanged,sendSignInLinkToEmail,signInWithEmailLink,isSignInWithEmailLink,createUserWithEmailAndPassword,signInWithEmailAndPassword,sendPasswordResetEmail,sendEmailVerification}from"https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";import{getFirestore,serverTimestamp,Timestamp,collection,doc,addDoc,setDoc,getDocs,getDoc,updateDoc,deleteDoc,writeBatch,deleteField,FieldPath,query,where,documentId,orderBy,startAt,endBefore,limit,getCountFromServer}from"https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";import{getStorage,ref,uploadBytes,getBytes,getMetadata,getDownloadURL,list,listAll,deleteObject}from"https://www.gstatic.com/firebasejs/10.7.0/firebase-storage.js";const API_KEY="AIzaSyDtn21mi-DzG-TGErzkkaNqUifB-cC1KQQ",firebaseConfig={apiKey:API_KEY,authDomain:"lectureseats.firebaseapp.com",projectId:"lectureseats",storageBucket:"lectureseats.appspot.com",messagingSenderId:"911998665124",appId:"1:911998665124:web:27b4b44bcc6cd3b7054131",measurementId:"G-VT95CQX9DC"},app=initializeApp(firebaseConfig),analytics=getAnalytics(app);window.auth=getAuth(),window.onAuth=e=>onAuthStateChanged(window.auth,e),window.isSignInWithEmailLink=isSignInWithEmailLink,window.passwordReset=function(e){return sendPasswordResetEmail(window.auth,e+DOMAIN)},window.signIn=function(e,t){return signInWithEmailAndPassword(window.auth,e+DOMAIN,t)},window.signUp=function(e,t){return createUserWithEmailAndPassword(window.auth,e.toLowerCase()+DOMAIN,t)},window.sendEmailLink=async function(e){try{await sendSignInLinkToEmail(window.auth,e+DOMAIN,{url:window.location.href,handleCodeInApp:!0})}catch(e){return{error:e.code}}},window.trySigningInWithLink=async function(e,t){try{return await signInWithEmailLink(window.auth,t+DOMAIN,e)}catch(e){return{error:e.code}}},window.sendEmailVerification=sendEmailVerification;const storage=getStorage();var downloadURLs={};async function copyFile(e,t){let r=await getMetadata(e),o=await getBytes(e);return await uploadBytes(t,o,r)}async function getOneFileInPath(e){if(!navigator.onLine)return{error:"You are offline."};try{let t=await listAll(ref(storage,e));if(t.items.length)return t.items[0];for(let e of t.prefixes){let t=getOneFileInPath(e);if(t)return t}}catch(e){return{error:e.code}}}window.downloadURLs=downloadURLs,window.storage=storage,window.storageRef=e=>ref(storage,e),window.uploadFile=async function(e,t){if(!navigator.onLine)return{error:"You are offline."};try{return await uploadBytes(ref(storage,e),t)}catch(e){return{error:e.code}}},window.moveFiles=async function(e,t){if(!navigator.onLine)return{error:"You are offline."};let r=new Set,o=new Set,n=0,i=0,a={ok:0,okCopy:0};for(let d of(await list(ref(storage,e))).items){try{await copyFile(d,ref(storage,t+"/"+d.name)),a.okCopy++,delete downloadURLs[e+"/"+d.name]}catch(e){n++,r.add(e.code)}try{await deleteObject(d),a.ok++}catch(e){i++,o.add(e.code)}}return n&&(a.error=`Failed to copy ${n} of ${n+a.okCopy} files (${[...r]})`),i&&(a.warn=`Failed to delete ${i} of the old files after copying (${[...o]})`),a},window.deleteFile=async function(e){if(!navigator.onLine)return{error:"You are offline."};try{return await deleteObject(ref(storage,e)),{ok:1}}catch(e){return"storage/object-not-found"===e.code?{ok:0}:{error:e.code}}},window.deleteFiles=async function(e){if(!navigator.onLine)return{error:"You are offline."};let t=[],r=new Set,o={};for(let o of(await list(ref(storage,e))).items)try{t.push(await deleteObject(o))}catch(e){r.add(e.code)}return t.length&&(o.ok=t.length),1===r.size?o.error=[...r][0]:r.size&&(o.error=[...r]),o},window.getLinkFromStoragePath=async function(e){if(!navigator.onLine)return{error:"You are offline."};if(e)try{let t=downloadURLs[e];return t||(t=downloadURLs[e]=await getDownloadURL(ref(storage,e))),t}catch(e){return{error:e.code}}},window.getOneLinkFromStoragePath=async function(e){if(!navigator.onLine)return{error:"You are offline."};if(e)try{for(let t in downloadURLs)if(t.startsWith(e+"/"))return downloadURLs[t];let t=await getOneFileInPath(e);return!t||t.error?t:(downloadURLs[t.fullPath]=await getDownloadURL(t),downloadURLs[t.fullPath])}catch(e){return{error:e.code}}};const DATABASE=getFirestore(app);window.db={},window.db.serverTimestamp=serverTimestamp,window.db.Timestamp=Timestamp,window.db.now=()=>Timestamp.now().seconds,window.db.makeTimestamp=function(e,t=0){return e.constructor===Array&&(e=e.join(" ")),db.Timestamp.fromMillis(new Date(e).getTime()+6e4*t)},window.db.fieldPath=(...e)=>new FieldPath(...e),window.db.deleteField=deleteField,window.db.where=where,window.db.orderBy=orderBy,window.db.startAt=startAt,window.db.endBefore=endBefore,window.db.limit=limit,window.db.documentId=documentId,window.db.startsWith=(e,t)=>[where(e,">=",t),where(e,"<",t+"")],window.db.count=async function(e,...t){try{let r;return r=t?await getCountFromServer(query(collection(DATABASE,e),...t)):await getCountFromServer(collection(DATABASE,e)),r.data().count}catch(e){return{error:e.code||"unknown error"}}},window.db.find=async function(e,...t){if(!navigator.onLine)return{error:"You are offline."};try{if(t?.length)var r=await getDocs(query(collection(DATABASE,e),...t));else r=await getDocs(collection(DATABASE,e));let o={};return r.docs.forEach((e=>o[e.id]=e.data())),o}catch(e){return{error:e.code||"unknown error"}}},window.db.getDoc=async function(e,t){if(!navigator.onLine)return{error:"You are offline."};try{var r=await getDoc(doc(DATABASE,e,t));return r.exists()?r.data():null}catch(e){return{error:e.code||"unknown error"}}},window.db.deleteOne=async function(e,t){if(!navigator.onLine)return{error:"You are offline."};try{await deleteDoc(doc(DATABASE,e,t))}catch(e){return{error:e.code||"unknown error"}}},window.db.insertOne=async function(e,t,r){if(!navigator.onLine)return{error:"You are offline."};try{if(t){await setDoc(doc(DATABASE,e,t),r);return t}return(await addDoc(collection(DATABASE,e),r)).id}catch(e){return{error:e.code||"unknown error"}}},window.db.updateOne=async function(e,t,r,...o){if(!navigator.onLine)return{error:"You are offline."};try{return o.length?await updateDoc(doc(DATABASE,e,t),r,...o):await updateDoc(doc(DATABASE,e,t),r),!0}catch(e){return{error:e.code||"unknown error"}}},window.db.updateBatch=async function(...e){if(!navigator.onLine)return{error:"You are offline."};try{const t=writeBatch(DATABASE);for(let r of e)t.update(doc(DATABASE,r.collection,r.id),r.data);return await t.commit(),!0}catch(e){return{error:e.code||"unknown error"}}},window.db.upsertOne=async function(e,t,r){if(!navigator.onLine)return{error:"You are offline."};try{return await setDoc(doc(DATABASE,e,t),r,{merge:!0}),!0}catch(e){return{error:e.code||"unknown error"}}},window.db.upsertBatch=async function(...e){if(!navigator.onLine)return{error:"You are offline."};try{const t=writeBatch(DATABASE);for(let r of e)t.set(doc(DATABASE,r.collection,r.id),r.data,{merge:!0});return await t.commit(),!0}catch(e){return{error:e.code||"unknown error"}}};
+// import required firebase functions
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-analytics.js";
+import { getAuth, onAuthStateChanged, sendSignInLinkToEmail, signInWithEmailLink, isSignInWithEmailLink, createUserWithEmailAndPassword, signInWithEmailAndPassword, sendPasswordResetEmail, sendEmailVerification } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
+import { getFirestore, serverTimestamp, Timestamp, collection, doc, addDoc, setDoc, getDocs, getDoc, updateDoc, deleteDoc, writeBatch, deleteField, FieldPath, query, where, documentId, orderBy, startAt, endBefore, limit, getCountFromServer } from 'https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js';
+import { getStorage, ref, uploadBytes, getBytes, getMetadata, getDownloadURL, list, listAll, deleteObject } from 'https://www.gstatic.com/firebasejs/10.7.0/firebase-storage.js';
+
+
+// domain restriction
+
+
+// firebase configuration
+const API_KEY = "AIzaSyDtn21mi-DzG-TGErzkkaNqUifB-cC1KQQ";
+const firebaseConfig = {
+  apiKey: API_KEY,
+  authDomain: "lectureseats.firebaseapp.com",
+  projectId: "lectureseats",
+  storageBucket: "lectureseats.appspot.com",
+  messagingSenderId: "911998665124",
+  appId: "1:911998665124:web:27b4b44bcc6cd3b7054131",
+  measurementId: "G-VT95CQX9DC",
+  // clientId: "911998665124-be5qcdaj4co3082a7kcn6so1638itpjo.apps.googleusercontent.com",
+};
+
+// init firebase
+const app = initializeApp(firebaseConfig);
+
+const analytics = getAnalytics(app);
+
+const DOMAIN='@caldwell.edu';
+
+//////////////// auth operations /////////////////////////
+
+// init authentication
+window.auth = getAuth();
+
+// bind global onAuth to onAuthStateChanged
+window.onAuth = f => onAuthStateChanged(window.auth, f);
+
+window.isSignInWithEmailLink=isSignInWithEmailLink;
+
+window.passwordReset=function(email){
+  return sendPasswordResetEmail(window.auth, email+DOMAIN);
+}
+
+window.signIn=function(email, password){
+  return signInWithEmailAndPassword(window.auth, email+DOMAIN, password);
+}
+
+window.signUp=function(email, password){
+  return createUserWithEmailAndPassword(window.auth, email.toLowerCase()+DOMAIN, password);
+}
+
+window.sendEmailLink=async function(email){
+  try{
+    await sendSignInLinkToEmail(window.auth, email+DOMAIN, {url:window.location.href, handleCodeInApp:true});
+  }catch(e){
+    console.error(e);
+    return {error:e.code};
+  }
+}
+
+window.trySigningInWithLink=async function(path,email){
+  try{
+    let r=await signInWithEmailLink(window.auth,email+DOMAIN,path);
+    return r;
+  }catch(e){
+    return {error:e.code};
+  }
+}
+
+window.sendEmailVerification=sendEmailVerification;
+
+
+////////////// storage operations ////////////////////////
+
+const storage = getStorage();
+var downloadURLs={};
+window.downloadURLs=downloadURLs;
+window.storage=storage;
+window.storageRef=path=>ref(storage,path);
+// window.updateMetadata=updateMetadata;
+
+window.uploadFile = async function(filepath,file){
+  if(!navigator.onLine)return {error:'You are offline.'};
+  try{
+    return await uploadBytes(ref(storage,filepath),file);
+  }catch(e){
+    return {error:e.code};
+  }
+}
+
+async function copyFile(oldRef,newRef){
+  let metadata=await getMetadata(oldRef);
+  let bytes = await getBytes(oldRef);
+  let r = await uploadBytes(newRef,bytes,metadata);
+  return r;
+}
+
+window.moveFiles = async function(oldPath,newPath){
+  if(!navigator.onLine)return {error:'You are offline.'};
+  let errorTypes=new Set(), warnTypes=new Set(), failCopy=0, failDel=0, r={ok:0,okCopy:0};
+  for(let fileRef of (await list(ref(storage,oldPath))).items){
+    try{
+      await copyFile(fileRef,ref(storage,newPath+'/'+fileRef.name));
+      r.okCopy++;
+      delete downloadURLs[oldPath+'/'+fileRef.name];
+    }catch(e){
+      failCopy++;
+      errorTypes.add(e.code);
+    }
+    try{
+      await deleteObject(fileRef);
+      r.ok++;
+    }catch(e){
+      failDel++;
+      warnTypes.add(e.code);
+    }
+  }
+  if(failCopy){
+    r.error=`Failed to copy ${failCopy} of ${failCopy+r.okCopy} files (${[...errorTypes]})`;
+  }
+  if(failDel){
+    r.warn=`Failed to delete ${failDel} of the old files after copying (${[...warnTypes]})`;
+  }
+  return r;
+}
+
+window.deleteFile = async function(filepath){
+  if(!navigator.onLine)return {error:'You are offline.'};
+  try{
+    await deleteObject(ref(storage,filepath));
+    console.log('Deleted',filepath);
+    return {ok:1};
+  }catch(e){
+    if(e.code==='storage/object-not-found'){
+      console.warn('No such file',filepath);
+      return {ok:0};
+    }
+    console.error('Could not delete',filepath,e);
+    return {error:e.code};
+  }
+}
+
+window.deleteFiles = async function(folderPath){
+  if(!navigator.onLine)return {error:'You are offline.'};
+  let ok=[], errors=new Set(), r={};
+  for(let f of (await list(ref(storage,folderPath))).items){
+    try{
+      ok.push(await deleteObject(f));
+      console.log(`Deleted ${folderPath}/${f.name}`);
+    }catch(e){
+      errors.add(e.code);
+      console.error(`Couldn't delete ${folderPath}/${f.name}`,e);
+    }
+  }
+  if(ok.length)r.ok=ok.length;
+  if(errors.size===1)r.error=[...errors][0];
+  else if(errors.size)r.error=[...errors];
+  return r;
+}
+
+window.getLinkFromStoragePath = async function(filepath){
+  if(!navigator.onLine)return {error:'You are offline.'};
+  if(!filepath)return;
+  try{
+    let r=downloadURLs[filepath];
+    if(!r){
+      r=downloadURLs[filepath]=await getDownloadURL(ref(storage,filepath));
+    }
+    return r;
+  }catch(e){
+    return {error:e.code};
+  }
+}
+
+// async function listRecursively(filepath){
+//   try{
+//     let r=await listAll(ref(storage,filepath));
+//     return r.items.concat(await Promise.all(r.prefixes.map(p=>listRecursively(p)))).flat();
+//   }catch(e){
+//     let r=[];
+//     r.error=e.code;
+//     return r;
+//   }
+// }
+
+// window.getLinksFromStoragePath = async function(filepath){
+//   if(!filepath)return;
+//   let r=await listRecursively(filepath);
+//   let links=[];
+//   for(let fileref of r){
+//     links.push(downloadURLs[fileref.fullPath]=await getDownloadURL(fileref));
+//   }
+//   console.log(downloadURLs)
+//   return links;
+// }
+
+async function getOneFileInPath(path){
+  if(!navigator.onLine)return {error:'You are offline.'};
+  try{
+    let r=await listAll(ref(storage,path));
+    if(r.items.length){
+      return r.items[0];
+    }else{
+      for(let folder of r.prefixes){
+        let r=getOneFileInPath(folder);
+        if(r)return r;
+      }
+    }
+  }catch(e){
+    return {error:e.code};
+  }
+}
+
+window.getOneLinkFromStoragePath = async function(path){
+  if(!navigator.onLine)return {error:'You are offline.'};
+  if(!path)return;
+  try{
+    for(let fullPath in downloadURLs){
+      if(fullPath.startsWith(path+'/')){
+        return downloadURLs[fullPath];
+      }
+    }
+    let r=await getOneFileInPath(path);
+    if(!r||r.error)return r;
+    downloadURLs[r.fullPath]=await getDownloadURL(r);
+    return downloadURLs[r.fullPath];
+  }catch(e){
+    return {error:e.code};
+  }
+}
+
+
+////////////// firestore operations //////////////////////
+
+const DATABASE = getFirestore(app);
+
+window.db = {};
+
+window.db.serverTimestamp=serverTimestamp;
+window.db.Timestamp=Timestamp;
+window.db.now=()=>Timestamp.now().seconds;
+
+window.db.makeTimestamp=function(dateTime,minutesOffset=0){
+  if(dateTime.constructor===Array)dateTime=dateTime.join(' ');
+  return db.Timestamp.fromMillis((new Date(dateTime).getTime()+minutesOffset*60000));
+}
+
+window.db.fieldPath=(...path)=>new FieldPath(...path);
+window.db.deleteField = deleteField;
+window.db.where = where;
+window.db.orderBy = orderBy;
+window.db.startAt = startAt;
+window.db.endBefore = endBefore;
+window.db.limit = limit;
+window.db.documentId = documentId;
+
+window.db.startsWith=(field,txt)=>[where(field,'>=',txt),where(field,'<',txt+'\uf8ff')];
+
+// Ex: cnt=await db.count('users',[db.documentId(),'>=','v'],[db.documentId(),'<','w'])
+window.db.count = async function(collectionName, ...constraints){
+  try{
+    let r;
+    if(constraints){
+      r=await getCountFromServer(query(collection(DATABASE, collectionName), ...constraints));
+    }else{
+      r=await getCountFromServer(collection(DATABASE, collectionName));
+    }
+    return r.data().count;
+  }catch(e){
+    return {error:e.code||'unknown error'};
+  }
+}
+
+// Ex: docs=await db.find('users',[db.documentId(),'>=','v'],[db.documentId(),'<','w'])
+window.db.find = async function(collectionName, ...constraints){
+  if(!navigator.onLine)return {error:'You are offline.'};
+  try{
+    if(constraints?.length){
+      var r=await getDocs(query(collection(DATABASE, collectionName), ...constraints));
+    }else{
+      var r=await getDocs(collection(DATABASE, collectionName));
+    }
+    let resultMap={};
+    r.docs.forEach(doc=>resultMap[doc.id]=doc.data());
+    return resultMap;
+  }catch(e){
+    console.error(e);
+    // toast(e.code,'Error');
+    return {error:e.code||'unknown error'};
+  }
+}
+
+window.db.getDoc = async function(collectionName, id){
+  if(!navigator.onLine)return {error:'You are offline.'};
+  try{
+    var docSnap = await getDoc(doc(DATABASE, collectionName, id));
+    return docSnap.exists()?docSnap.data():null;
+  }catch(e){
+    console.error(e);
+    return {error:e.code||'unknown error'};
+  }
+}
+
+window.db.deleteOne = async function(collectionName, id){
+  if(!navigator.onLine)return {error:'You are offline.'};
+  try{
+    await deleteDoc(doc(DATABASE, collectionName, id));
+  }catch(e){
+    console.error(e);
+    return {error:e.code||'unknown error'};
+  }
+}
+
+window.db.insertOne = async function(collectionName, id, data){
+  if(!navigator.onLine)return {error:'You are offline.'};
+  try{
+    if(id){
+      var docRef = await setDoc(doc(DATABASE, collectionName, id), data);
+      return id;
+    }else{
+      docRef = await addDoc(collection(DATABASE, collectionName), data);
+      return docRef.id;
+    }
+  }catch(e){
+    console.error(e);
+    return {error:e.code||'unknown error'};
+  }
+}
+
+window.db.updateOne = async function(collectionName, id, data, ...data2){
+  if(!navigator.onLine)return {error:'You are offline.'};
+  try{
+    if(data2.length){
+      await updateDoc(doc(DATABASE, collectionName, id), data, ...data2);
+    }else{
+      await updateDoc(doc(DATABASE, collectionName, id), data);
+    }
+    return true;
+  }catch(e){
+    console.error(e);
+    return {error:e.code||'unknown error'};
+  }
+}
+
+window.db.updateBatch = async function(...updates){
+  // each update in updates must be an object with fields {collection, id, data}
+  if(!navigator.onLine)return {error:'You are offline.'};
+  try{
+    const batch = writeBatch(DATABASE);
+    for(let update of updates){
+      batch.update(doc(DATABASE,update.collection,update.id),update.data);
+    }
+    await batch.commit();
+    return true;
+  }catch(e){
+    console.error(e);
+    return {error:e.code||'unknown error'};
+  }
+}
+
+window.db.upsertOne = async function(collectionName, id, data){
+  if(!navigator.onLine)return {error:'You are offline.'};
+  try{
+    await setDoc(doc(DATABASE, collectionName, id), data, {merge:true});
+    return true;
+  }catch(e){
+    return {error:e.code||'unknown error'};
+  }
+}
+
+window.db.upsertBatch = async function(...updates){
+  // each update in updates must be an object with fields {collection, id, data}
+  if(!navigator.onLine)return {error:'You are offline.'};
+  try{
+    const batch = writeBatch(DATABASE);
+    for(let update of updates){
+      batch.set(doc(DATABASE,update.collection,update.id),update.data,{merge:true});
+    }
+    await batch.commit();
+    return true;
+  }catch(e){
+    console.error(e);
+    return {error:e.code||'unknown error'};
+  }
+}
